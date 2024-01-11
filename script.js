f474029d9925e09a588e7beb7998ca7865227ffd
Abhinay Jangde <abhinayjangde@gmail.com>
2024-01-11 09:21:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Search Bar Buttom 
+const searchBox = document.querySelector(".search-box")
+const searchBtn = document.querySelector(".search-btn")
+const search = document.querySelector(".search")
+const closeBtn = document.querySelector(".close-btn")
+
+searchBtn.addEventListener('click',()=>{
+    if(search.classList.contains("active")){
+        searchBox.value=''
+    }
+    else{
+        search.classList.add("active")
+        searchBox.focus()
+    }
+})
+closeBtn.addEventListener("click",()=>{
+    search.classList.remove("active")
+    searchBox.value=''
+})
